4491c19a5058a1644b97db77aa35bce9534fb213
Gabriel H <gabriel.ghvn@gmail.com>
2021-12-22 15:02:28 -0300

    sketch 02 added

---
+++ sketch-02.js
@@ -0,0 +1,34 @@
+const canvasSketch = require('canvas-sketch');
+
+const settings = {
+  dimensions: [ 1080, 1080 ]
+};
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+    
+    context.fillStyle = 'black';
+    
+    const x = width  * 0.5;
+    const y = height * 0.5;
+    const w = width  * 0.3;
+    const h = height * 0.3;
+    
+    // Saving the current context
+    context.save();
+    // change it
+    context.translate(x, y);
+    context.rotate(0.3);
+
+    context.beginPath();
+    context.rect(-w / 2, -h / 2, w, h);
+    context.fill();
+    // restore the state saved
+    context.restore();
+
+  };
+};
+
+canvasSketch(sketch, settings);
